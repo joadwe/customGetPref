0fef62646110cf677481cfd0d445903133159c64
JDawg <JDawg@172.27.4.15>
2023-02-13 14:53:34 +0700

    - updated isprefc to allow to check whether preference directory exists with one input argument

--- source_code/isprefc.m
+++ source_code/isprefc.m
@@ -1,19 +1,22 @@
-function status = isprefc(PrefType, prefName)
+function status = isprefc(PrefType, varargin)
 
 % obtain filepath for preference file
 [filepath] = predirc(PrefType);
 
-% load preference file
-D = matfile(filepath);
-
-% obtain fieldnames
-savedPrefs = fieldnames(D);
-
-% check if specified preference exists in file structure
-if ismember(savedPrefs, prefName) > 0
-    status = true;
+if (nargin) == 1
+    status = isfile(filepath);
 else
-    status = false;
-end
+    % load preference file
+    D = matfile(filepath);
 
+    % obtain fieldnames
+    savedPrefs = fieldnames(D);
+
+    % check if specified preference exists in file structure
+    if ismember(savedPrefs, prefName) > 0
+        status = true;
+    else
+        status = false;
+    end
+end
 end
